3fec9c0abd0c77e6f09e94f2af85a9b85af9a1ad
Jim Jiang <jim.jiang0727@gmail.com>
2021-05-17 14:20:01 +1000

    add login page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-    <h1>Todo App</h1>
-    <input type="text" v-model="name" placeholder="Todo name">
-    <input type="text" v-model="description" placeholder="Todo description">
-    <button v-on:click="createTodo">Create Todo</button>
-    <div v-for="item in todos" :key="item.id">
-      <h3>{{ item.name }}</h3>
-      <p>{{ item.description }}</p>
-    </div>
+    <amplify-authenticator>
+      <h1>Todo App</h1>
+      <input type="text" v-model="name" placeholder="Todo name" />
+      <input type="text" v-model="description" placeholder="Todo description" />
+      <button v-on:click="createTodo">Create Todo</button>
+      <div v-for="item in todos" :key="item.id">
+        <h3>{{ item.name }}</h3>
+        <p>{{ item.description }}</p>
+      </div>
+      <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
   </div>
 </template>
 
@@ -25,8 +28,8 @@ export default {
     return {
       name: '',
       description: '',
-      todos: []
-    }
+      todos: [],
+    };
   },
   methods: {
     async createTodo() {
@@ -36,17 +39,17 @@ export default {
       this.todos = [...this.todos, todo];
       await API.graphql({
         query: createTodo,
-        variables: {input: todo},
+        variables: { input: todo },
       });
       this.name = '';
       this.description = '';
     },
     async getTodos() {
       const todos = await API.graphql({
-        query: listTodos
+        query: listTodos,
       });
       this.todos = todos.data.listTodos.items;
-    }
-  }
-}
+    },
+  },
+};
 </script>

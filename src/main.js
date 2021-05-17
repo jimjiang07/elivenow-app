ac91122cb429f4c02915bb944e6f59b52d68e853
Jim Jiang <jim.jiang0727@gmail.com>
2021-05-17 11:44:35 +1000

    todo tutorial

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
 import {
   applyPolyfills,
   defineCustomElements,
 } from '@aws-amplify/ui-components/loader';
+import { createApp } from 'vue';
+import aws_exports from './aws-exports';
+import App from './App.vue';
 
 Amplify.configure(aws_exports);
 applyPolyfills().then(() => {
   defineCustomElements(window);
 });
+
+createApp(App).mount('#app');
